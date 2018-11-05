4bc25ee0136583cb2b3b0426356b8fe79965c55a
Johannes Hellrich <johannes.hellrich@uni-jena.de>
2018-11-05 13:34:42 +0100

    Added debugging, output to file instead of stdout

--- hyperwords/corpus2counts.py
+++ hyperwords/corpus2counts.py
@@ -19,48 +19,44 @@ def main():
         --ww         Use weighted window (exclusive with --dw)
         --psub NUM   Probabilistic subsampling threshold (exclusive with --dsub) [default: 0]
         --dsub NUM   Deterministic subsampling threshold (exclusive with --psub) [default: 0]
-        --pairs FILE  Stores pairs in FILE [default: False]
-        --debug      Print debugging info to STDERR
+        --pairs FILE Stores pairs in FILE [default: False]
+        --out FILE   Stores count output in FILE [default: False]
+        --debug FILE Store debugging info in FILE [default: False]
     """)
 
     corpus_file = args['<corpus>']
     thr = int(args['--thr'])
-    pairs= args["--pairs"]
-    if pairs == "False":
-        pairs = False
-    else:
-        pairs=open(pairs, "w")
+    pairs = open_if_not_false(args["--pairs"])
+    out = open_if_not_false(args["--out"])
+    debug = open_if_not_false(args["--debug"])
     win = int(args['--win'])
     dynamic_window = args["--dw"]
     weighted_window = args["--ww"]
-    debug=args["--debug"]
     if dynamic_window and weighted_window:
         raise Exception("Dynamic and weighted window options are exclusive!")
-    psubsample = float(args['--psub'])
-    dsubsample = float(args['--dsub'])
+    psubsample = float(args['--psub']) #probabilistic
+    dsubsample = float(args['--dsub']) #deterministic
     if psubsample and dsubsample:
         raise Exception("Subsampling options are exclusive!")
 
 
     vocab = read_vocab(corpus_file, thr, debug)
-    corpus_size = sum(vocab.values())
+    corpus_size = sum(vocab.itervalues())
     if psubsample:
         subsample = psubsample * corpus_size
     elif dsubsample:
         subsample = dsubsample * corpus_size
     if psubsample or dsubsample:
         #changed to prob from 1-prob !
-        subsampler = dict([(word, sqrt(subsample / count)) for word, count in vocab.items() if count > subsample])
+        subsampler = {word : sqrt(subsample / count) for word, count in vocab.iteritems() if count > subsample}
 
 
     rnd = Random()
     counts = Counter()
     with open(corpus_file) as f:
-        if debug:
-            print("Creating counts")
         for line in f:
             if debug:
-                print("Processing: "+line, file=sys.stderr)
+                print("Processing4counts: "+line, file=debug)
             #dirty subsampling, results in vales lower than dsub!
             tokens = [t if t in vocab else None for t in line.strip().split()]
             if psubsample:
@@ -90,22 +86,32 @@ def main():
                                 tok2_factor = subsampler[tokens[j]] if tokens[j] in subsampler else 1
                                 count = tok1_factor * tok2_factor * count
                             if pairs:
-                                pairs.write(str(count)+" "+ tok + " " + tokens[j]+"\n")
+                                print(str(count)+" "+ tok + " " + tokens[j], file=pairs)
+                                #pairs.write(str(count)+" "+ tok + " " + tokens[j]+"\n")
                             counts[(tok, tokens[j])] += count
-    for pair, count in counts.items():
-        print(str(count)+" "+pair[0] + " " + pair[1])
+    if not out:
+        out = file=sys.stdout #somehow not working properly?
+    for pair, count in counts.iteritems():
+        print(str(count)+" "+pair[0] + " " + pair[1], file=out)
 
+def open_if_not_false(name):
+    if name == None:
+        return False
+    return open(name, "w")
 
 def read_vocab(corpus_file, thr, debug=False):
     vocab = Counter()
     with open(corpus_file) as f:
-        if debug:
-            print("Creating vocabulary")
         for line in f:
             if debug:
-                print("Processing: "+line, file=sys.stderr)
+                print("Processing4vocab: "+line, file=debug)
             vocab.update(Counter(line.strip().split()))
-    return dict([(token, count) for token, count in vocab.items() if count >= thr])
+    if debug:
+        print("Types: "+str(len(vocab)), file=sys.stderr)
+    vocab = {token : count for token, count in vocab.iteritems() if count >= thr}
+    if debug:
+        print("Types over threshold: "+str(len(vocab)), file=sys.stderr)
+    return vocab
 
 
 if __name__ == '__main__':
